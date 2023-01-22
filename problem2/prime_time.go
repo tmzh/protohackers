4dae269eeb1412a5b83a0dd7ffbcc89d885a564d
tmzh <2162408+tmzh@users.noreply.github.com>
2023-01-22 16:27:29 +0800

    problem 2 solved

--- problem2/prime_time.go
+++ problem2/prime_time.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -9,7 +11,8 @@ import (
 	"os"
 )
 
-func isPrime(n int) bool {
+func isPrime(f float64) bool {
+	n := int(f)
 	if n <= 3 {
 		return n > 1
 	}
@@ -27,34 +30,77 @@ func isPrime(n int) bool {
 	return true
 }
 
-func parseMessage() {
+func isMalformed(message *Message) bool {
+	if message.Method == nil || *message.Method != "isPrime" || message.Number == nil {
+		b, _ := json.Marshal(message)
+		log.Printf("Message is: %s\n", string(b))
+		log.Printf("Returning response: %s\n", "malformed")
+		return true
+	}
+	return false
+}
+
+type Message struct {
+	Method *string  `json:"method"`
+	Number *float64 `json:"number"`
+}
+
+type Response struct {
+	Method string `json:"method"`
+	Prime  bool   `json:"prime"`
+}
+
+func handleConnection(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		var message *Message
+
+		err := json.Unmarshal(scanner.Bytes(), &message)
+		if err != nil || isMalformed(message) {
+			log.Printf("Received line: %s\n", scanner.Text())
+			log.Printf("Error in JSON: %s\n", err)
+			log.Printf("Returning response: %s\n", "malformed")
+			conn.Write([]byte("malformed"))
+			conn.Close()
+		} else {
+			response := Response{
+				Method: "isPrime",
+				Prime:  isPrime(*message.Number),
+			}
+
+			b, _ := json.Marshal(response)
+			log.Printf("Returning response: %s\n", string(b))
+			conn.Write(b)
+			conn.Write([]byte{10})
+
+		}
+	}
 
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":9000")
+	ln, err := net.Listen("tcp", ":9000")
+
 	if err != nil {
 		fmt.Println("Cannot create listener, err:", err)
 		os.Exit(1)
 	}
-	log.Printf("Listening on %s\n", listener.Addr())
+	log.Printf("Listening on %s\n", ln.Addr())
 
 	for {
-		conn, err := listener.Accept()
+		conn, err := ln.Accept()
 		if err != nil {
 			if errors.Is(err, net.ErrClosed) {
 				log.Printf("Connection closed\n")
 				return
 			}
-
 			log.Printf("Failed to accept incoming connection\n")
 			continue
 		}
 
-		go func(c net.Conn) {
-			b, err := io.ReadAll
+		go handleConnection(conn)
 
-		}(conn)
 	}
 
 }
--- problem2/prime_time_test.go
+++ problem2/prime_time_test.go
@@ -5,15 +5,15 @@ import "testing"
 func Test_isPrime(t *testing.T) {
 	tests := []struct {
 		name string
-		n    int
+		n    float64
 		want bool
 	}{
-		{"one", 1, false},
-		{"two", 2, true},
-		{"three", 3, true},
-		{"four", 4, false},
-		{"five", 5, true},
-		{"seventeen", 17, true},
+		{"one", 1.0, false},
+		{"two", 2.0, true},
+		{"three", 3.0, true},
+		{"four", 4.0, false},
+		{"five", 5.0, true},
+		{"seventeen", 17.0, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
